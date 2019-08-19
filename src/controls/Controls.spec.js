a07fa77110b6d4a5bc599967ebb3306c61d40c5a
Scott Renz <scottrenz@scottrenz.com>
2019-08-19 19:05:05 -0400

    trying to make test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,34 @@
-// Test away!
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Controls from './Controls';
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Controls />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+// // Test away!
+// describe("Control", () => {
+//     it('Should pass "Lock', () => {
+//       const { queryByText } = render(<Controls />);
+  
+//       expect(queryByText(/Unlock/i)).toBeFalsy();
+  
+//       fireEvent.click(getByText(/Gate/i));
+  
+//       expect(queryByText(/Lock/i)).toBeTruthy();
+//     });
+//   });
+
+// import Controls from './Controls';
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+
+
+// describe('<Controls />', () => {
+//   it('should match snapshot', () => {
+//     const tree = renderer.create(<Controls />).toJSON();
+
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+import Dashboard from './Dashboard';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+
+describe('<Dashboard />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
