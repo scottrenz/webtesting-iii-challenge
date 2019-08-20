5e05433dc9d4818d2b9d275d5502061993e23cfe
Scott Renz <scottrenz@scottrenz.com>
2019-08-19 20:25:18 -0400

    got snap shots to work. How? I don't know. Suddenly, they were working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,13 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Controls from './Controls';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Controls from './Controls';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<Controls />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
-// // Test away!
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Controls />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+// Test away!
 // describe("Control", () => {
 //     it('Should pass "Lock', () => {
 //       const { queryByText } = render(<Controls />);
@@ -22,13 +22,72 @@
 
 // import Controls from './Controls';
 // import React from 'react';
+import renderer from 'react-test-renderer';
+describe('<Controls />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Controls />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+// ////////////////////////////////
+// import React from 'react';
+import { render, fireEvent,} from '@testing-library/react'
+import * as rtl from '@testing-library/react';
 // import renderer from 'react-test-renderer';
+// import Controls from "./Controls";
+describe("<Controls />", () => {
+    it("defaults to unlocked and open", async () => {
+        const wrapper = rtl.render(<Controls locked={false} closed={false}/>);
+        let closebtn = wrapper.getByTestId('toggle-closed');
+        let lockbtn = wrapper.getByTestId('toggle-locked');
+        expect(closebtn).toBeDefined();
+        expect(lockbtn).toBeDefined();
+    })
+    it("cannot be closed or opened if it is locked",async () =>
+{
+        let wrapper = rtl.render(<Controls locked={true} closed={true}/>);
+        let button = wrapper.getByTestId('toggle-closed');
+        await rtl.fireEvent.click(button);
+        expect(button.disabled).toBe(true);
+        wrapper = rtl.render(<Controls locked={false} closed={false}/>);
+        button = wrapper.getByTestId('toggle-locked');
+        await rtl.fireEvent.click(button);
+        expect(button.disabled).toBe(true);
+    })
+})
+
+// ///////////////////////////////
 
 
-// describe('<Controls />', () => {
-//   it('should match snapshot', () => {
-//     const tree = renderer.create(<Controls />).toJSON();
+// import React from 'react';
 
-//     expect(tree).toMatchSnapshot();
-//   });
-// });
+// ​
+
+// const Controls = props => {
+
+//   const { locked, closed, toggleLocked, toggleClosed } = props;
+
+// ​
+
+//   return (
+
+//     <div className="controls panel">
+
+//       <button  data-testid='toggle-locked' disabled={!closed} onClick={toggleLocked} className="toggle-btn">
+
+//         {locked ? 'Unlock Gate' : 'Lock Gate'}
+
+//       </button>
+
+//       <button  data-testid='toggle-closed' disabled={locked} onClick={toggleClosed} className="toggle-btn">
+
+//         {closed ? 'Open Gate' : 'Close Gate'}
+
+//       </button>
+
+//     </div>
+
+//   );
+
+// };
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,13 +1,17 @@
-// Test away
-import Dashboard from './Dashboard';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Dashboard from './Dashboard';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Dashboard />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+// Test away!
 import renderer from 'react-test-renderer';
-
-
 describe('<Dashboard />', () => {
   it('should match snapshot', () => {
     const tree = renderer.create(<Dashboard />).toJSON();
-
     expect(tree).toMatchSnapshot();
   });
 });
